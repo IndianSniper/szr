573f748dcd09cc40cd05c06c2eb1c0ddbbf1e752
promack <mukesh.singh492@gmail.com>
2017-03-05 22:49:23 +0530

    added css images and js

---
+++ js/script.js
@@ -0,0 +1,315 @@
+(function($) {
+	
+	"use strict";
+	
+	
+	//Hide Loading Box (Preloader)
+	function handlePreloader() {
+		if($('.preloader').length){
+			$('.preloader').delay(500).fadeOut(500);
+		}
+	}
+	
+	
+	//Update Header Style + Scroll to Top
+	function headerStyle() {
+		if($('.main-header').length){
+			var windowpos = $(window).scrollTop();
+			if (windowpos >= 50) {
+				$('.main-header').addClass('fixed-header');
+				$('.scroll-to-top').fadeIn(300);
+			} else {
+				$('.main-header').removeClass('fixed-header');
+				$('.scroll-to-top').fadeOut(300);
+			}
+		}
+	}
+	
+	headerStyle();
+	
+	
+	//Add Onepage Nav
+	if($('.one-page-nav').length){
+		$('.one-page-nav').onePageNav();
+	}
+	
+	
+	//Submenu Dropdown Toggle
+	if($('.main-header li.dropdown > ul').length){
+		$('.main-header li.dropdown').append('<div class="dropdown-btn"></div>');
+		
+		//Dropdown Button
+		$('.main-header li.dropdown .dropdown-btn').on('click', function() {
+			$(this).prev('ul').slideToggle(500);
+		});
+	}
+	
+	
+	//Main Slider
+	if($('.main-slider .tp-banner').length){
+
+		jQuery('.main-slider .tp-banner').show().revolution({
+		dottedOverlay:"none",
+		  delay:10000,
+		  startwidth:1200,
+		  startheight:750,
+		  hideThumbs:200,
+	
+		  thumbWidth:80,
+		  thumbHeight:50,
+		  thumbAmount:5,
+	
+		  navigationType:"none",
+		  navigationArrows:"0",
+		  navigationStyle:"preview4",
+	
+		  touchenabled:"on",
+		  onHoverStop:"off",
+	
+		  swipe_velocity: 0.7,
+		  swipe_min_touches: 1,
+		  swipe_max_touches: 1,
+		  drag_block_vertical: false,
+	
+		  parallax:"mouse",
+		  parallaxBgFreeze:"on",
+		  parallaxLevels:[7,4,3,2,5,4,3,2,1,0],
+	
+		  keyboardNavigation:"off",
+	
+		  navigationHAlign:"center",
+		  navigationVAlign:"bottom",
+		  navigationHOffset:0,
+		  navigationVOffset:20,
+	
+		  soloArrowLeftHalign:"left",
+		  soloArrowLeftValign:"bottom",
+		  soloArrowLeftHOffset:0,
+		  soloArrowLeftVOffset:0,
+	
+		  soloArrowRightHalign:"right",
+		  soloArrowRightValign:"bottom",
+		  soloArrowRightHOffset:0,
+		  soloArrowRightVOffset:0,
+	
+		  shadow:0,
+		  fullWidth:"off",
+		  fullScreen:"off",
+	
+		  spinner:"spinner4",
+	
+		  stopLoop:"off",
+		  stopAfterLoops:-1,
+		  stopAtSlide:-1,
+	
+		  shuffle:"off",
+	
+		  autoHeight:"off",
+		  forceFullWidth:"on",
+	
+		  hideThumbsOnMobile:"on",
+		  hideNavDelayOnMobile:1500,
+		  hideBulletsOnMobile:"on",
+		  hideArrowsOnMobile:"on",
+		  hideThumbsUnderResolution:0,
+	
+		  hideSliderAtLimit:0,
+		  hideCaptionAtLimit:0,
+		  hideAllCaptionAtLilmit:0,
+		  startWithSlide:0,
+		  videoJsPath:"",
+		  fullScreenOffsetContainer: ""
+	  });
+
+		
+	}
+	
+	
+	//Tabs Box
+	if($('.tabs-box').length){
+		
+		//Tabs
+		$('.tabs-box .tab-btn').on('click', function(e) {
+			e.preventDefault();
+			var target = $($(this).attr('href'));
+			$('.tabs-box .tab-btn').removeClass('active-btn');
+			$(this).addClass('active-btn');
+			$('.tabs-box .tab').fadeOut(0);
+			$('.tabs-box .tab').removeClass('active-tab');
+			$(target).fadeIn(300);
+			$(target).addClass('active-tab');
+			var windowWidth = $(window).width();
+			if (windowWidth <= 700) {
+				$('html, body').animate({
+				   scrollTop: $('.tabs-box').offset().top
+				 }, 1000);
+			}
+		});
+		
+	}
+	
+	
+	//Three Column Slider
+	if ($('.column-carousel.three-column').length) {
+		$('.column-carousel.three-column').owlCarousel({
+			loop:true,
+			margin:30,
+			nav:true,
+			autoplayHoverPause:false,
+			autoplay: 5000,
+			smartSpeed: 700,
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:2
+				},
+				800:{
+					items:2
+				},
+				1024:{
+					items:3
+				},
+				1100:{
+					items:3
+				}
+			}
+		});    		
+	}
+	
+	//Testimonials Slider (One COlumn Testimonials)
+	if ($('.testimonial-slider .slider').length) {
+		$('.testimonial-slider .slider').bxSlider({
+			adaptiveHeight: true,
+			auto:true,
+			controls: true,
+			pause: 5000,
+			speed: 500,
+			pager: false,
+			mode:'fade'
+		});	
+	}
+	
+	
+	//Video Gallery Slider
+	if ($('.video-gallery').length) {
+		var slider = new MasterSlider();
+		
+		slider.setup('video-gallery', {
+			width : 1000,
+			height : 532,
+			space : 5,
+			shuffle : true,
+			loop : true,
+			view : 'parallaxMask'
+		});
+		
+		slider.control('arrows');
+		slider.control('thumblist', {autohide : false,	dir : 'h'});    		
+	}
+	
+	
+	
+	//LightBox / Fancybox
+	if($('.lightbox-image').length) {
+		$('.lightbox-image').fancybox();
+	}
+	
+	
+	//Gallery Filters
+	if($('.filter-list').length){
+		$('.filter-list').mixItUp({});
+	}
+	
+	
+	//Contact Form Validation
+	if($('#contact-form').length){
+		$('#contact-form').validate({
+			rules: {
+				username: {
+					required: true
+				},
+				email: {
+					required: true,
+					email: true
+				},
+				subject: {
+					required: true
+				},
+				message: {
+					required: true
+				}
+			}
+		});
+	}
+	
+	
+	// Google Map Settings
+	if($('#map-location').length){
+		var map;
+		 map = new GMaps({
+			el: '#map-location',
+			zoom: 14,
+			scrollwheel:false,
+			//Set Latitude and Longitude Here
+			lat: -37.817085,
+			lng: 144.955631
+		  });
+		  
+		  //Add map Marker
+		  map.addMarker({
+			lat: -37.817085,
+			lng: 144.955631,
+			infoWindow: {
+			  content: '<p style="text-align:center;"><strong>Envato</strong><br>Melbourne VIC 3000, Australia</p>'
+			}
+		 
+		});
+	}
+	
+	
+	// Scroll to top
+	if($('.scroll-to-top').length){
+		$(".scroll-to-top").on('click', function() {
+		   // animate
+		   $('html, body').animate({
+			   scrollTop: $('html, body').offset().top
+			 }, 1000);
+	
+		});
+	}
+	
+	
+	// Elements Animation
+	if($('.wow').length){
+		var wow = new WOW(
+		  {
+			boxClass:     'wow',      // animated element css class (default is wow)
+			animateClass: 'animated', // animation css class (default is animated)
+			offset:       0,          // distance to the element when triggering the animation (default is 0)
+			mobile:       true,       // trigger animations on mobile devices (default is true)
+			live:         true       // act on asynchronously loaded content (default is true)
+		  }
+		);
+		wow.init();
+	}
+
+/* ==========================================================================
+   When document is Scrollig, do
+   ========================================================================== */
+	
+	$(window).on('scroll', function() {
+		headerStyle();
+	});
+	
+/* ==========================================================================
+   When document is loading, do
+   ========================================================================== */
+	
+	$(window).on('load', function() {
+		handlePreloader();
+	});
+	
+
+})(window.jQuery);
